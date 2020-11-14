689163b67d7ae4895d3713a98b0346f9485c1b4a
AMoore202 <alexandermoore202@gmail.com>
2020-11-13 19:57:00 -0500

    Finishing scraping of results page
    Finished scaping results pages to get links to each recipe.

--- MuffinRecipes.R
+++ MuffinRecipes.R
@@ -1,13 +1,30 @@
+# Load packages
 library(tidyverse)
 library(rvest)
 library(xml2)
+library(magrittr)
 
-Page1 <- read_html("https://www.allrecipes.com/search/results/?wt=muffin&page=1")
+# Function for scaping links on each page of seach results
+LinkExtractor <- function(nodes){
+  links <- map_chr(c(1:length(nodes)),~nodes[.x] %>%
+                     html_node(".grid-card-image-container") %>%
+                     html_children() %>% 
+                     extract(1) %>% 
+                     as.character() %>% 
+                     str_extract("https[:print:]+/\\\"") %>% 
+                     str_sub(end=-2L)
+  )
+  return(links)
+}
 
-Nodes <- Page1 %>% html_node("body") %>% 
+# Scape links from first 50 pages of muffins
+links <- map(c(1:50),~read_html(paste0("https://www.allrecipes.com/search/results/?wt=muffin&page=",.x)) %>%
+  html_node("body") %>% 
   html_node(".slider-container") %>% 
   html_node(".site-content") %>% 
   html_node("#main-content") %>% 
   html_node("#searchResultsApp") %>% 
   html_node("#fixedGridSection") %>% 
-  html_nodes('.fixed-recipe-card')
+  html_nodes('.fixed-recipe-card') %>% 
+  LinkExtractor()
+) %>% unlist()
